c8aa80b72ab588073cc3e6607f98eca864847b80
Mothupi Keenan <iamkeenan94@gmail.com>
2018-04-15 07:26:36 +0200

    Online Voting system with Ganache libraries added along with the crypto ballot classes

--- app/src/main/java/com/sirmothupii/encrypto/IECVotes.java
+++ app/src/main/java/com/sirmothupii/encrypto/IECVotes.java
@@ -1,7 +1,9 @@
 package com.sirmothupii.encrypto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class IECVotes extends AppCompatActivity
 {
@@ -11,5 +13,11 @@ public class IECVotes extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iecvotes);
+
+    }
+    public void onClickOnlineVotes(View view)
+    {
+        Intent intent1 = new Intent(this, CryptoBallot.class);
+        startActivity(intent1);
     }
 }
